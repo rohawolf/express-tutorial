60598266d3db1f4548a5e0e1cc647731205bcf1d
rohawolf <rohawolf@gmail.com>
2018-09-17 22:46:34 +0900

    REST API init

--- router/main.js
+++ router/main.js
@@ -1,9 +1,29 @@
 module.exports = function(app, fs) {
+
   app.get('/', (req, res) => {
     res.render('index', {
       title: "MY node.js",
       length: 5
     });
   });
+
+  app.get('/list', (req, res) => {
+    fs.readFile(__dirname + '/../data/user.json', 
+    'utf8',
+    (err, data) => {
+      console.log(data);
+      res.end(data);
+    });
+  });
+
+  app.get('/list/:username', (req, res) => {
+    fs.readFile(__dirname + '/../data/user.json',
+      'utf8',
+      (err, data) => {
+        const users = JSON.parse(data);
+        res.json(users[req.params.username]);
+      });
+  });
+
 }
   
