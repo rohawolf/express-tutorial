9837bc615e141f7cf58f77e9f2e4d485ec876210
rohawolf <rohawolf@gmail.com>
2018-09-17 23:10:52 +0900

    implement REST API

--- router/main.js
+++ router/main.js
@@ -7,7 +7,7 @@ module.exports = function(app, fs) {
     });
   });
 
-  app.get('/list', (req, res) => {
+  app.get('/users', (req, res) => {
     fs.readFile(__dirname + '/../data/user.json', 
     'utf8',
     (err, data) => {
@@ -16,7 +16,7 @@ module.exports = function(app, fs) {
     });
   });
 
-  app.get('/list/:username', (req, res) => {
+  app.get('/users/:username', (req, res) => {
     fs.readFile(__dirname + '/../data/user.json',
       'utf8',
       (err, data) => {
@@ -25,5 +25,114 @@ module.exports = function(app, fs) {
       });
   });
 
+  app.post('/users/add/:username', (req, res) => {
+    const result = {};
+    const username = req.params.username;
+    
+    //check validity
+    if(!req.body['password'] || !req.body['name']) {
+      result['success'] = 0;
+      result['error'] = 'invalid request';
+      res.json(result);
+      return;
+    }
+
+    //load data & check duplication
+    fs.readFile(__dirname + '/../data/user.json',
+      'utf8',
+      (err, data) => {
+        const users = JSON.parse(data);
+        if(users[username]) {
+          // duplicate!
+          result['success'] = 0;
+          result['error'] = 'duplicate';
+          res.json(result);
+          return;
+        }
+
+        //add data
+        users[username] = req.body;
+
+        //save data
+        fs.writeFile(__dirname + '/../data/user.json',
+          JSON.stringify(users, null, '\t'),
+          'utf8',
+          (err, data) => {
+            result['success'] = 1;
+            res.json(result);
+          });
+      });
+  });
+
+  app.put('/users/update/:username', (req, res) => {
+    const result = {};
+    const username = req.params.username;
+    
+    //check validity
+    if(!req.body['password'] || !req.body['name']) {
+      result['success'] = 0;
+      result['error'] = 'invalid request';
+      res.json(result);
+      return;
+    }
+
+    //load data & check duplication
+    fs.readFile(__dirname + '/../data/user.json',
+      'utf8',
+      (err, data) => {
+        const users = JSON.parse(data);
+        if(!users[username]) {
+          // not fount!
+          result['success'] = 0;
+          result['error'] = 'not fount';
+          res.json(result);
+          return;
+        }
+
+        //update data
+        users[username] = req.body;
+
+        //save data
+        fs.writeFile(__dirname + '/../data/user.json',
+          JSON.stringify(users, null, '\t'),
+          'utf8',
+          (err, data) => {
+            result['success'] = 1;
+            res.json(result);
+          });
+      });
+  });
+
+  app.delete('/users/delete/:username', (req, res) => {
+    const result = {};
+    const username = req.params.username;
+
+    //load data & check duplication
+    fs.readFile(__dirname + '/../data/user.json',
+      'utf8',
+      (err, data) => {
+        const users = JSON.parse(data);
+        
+        // not fount!
+        if(!users[username]) {
+          result['success'] = 0;
+          result['error'] = 'not fount';
+          res.json(result);
+          return;
+        }
+
+        //update data
+        delete users[username];
+
+        //save data
+        fs.writeFile(__dirname + '/../data/user.json',
+          JSON.stringify(users, null, '\t'),
+          'utf8',
+          (err, data) => {
+            result['success'] = 1;
+            res.json(result);
+          });
+      });
+  });
 }
   
