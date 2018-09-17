69d437659298d513e2c1587b0357f120594b7c85
rohawolf <rohawolf@gmail.com>
2018-09-17 22:14:38 +0900

    add router, views & ejs engine to render html

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const router = require('./router/main')(app);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 const server = app.listen(3000, () => {
    console.log("Express server has started on port 3000"); 
-});
-
-
-app.get('/', (req, res) => {
-    res.send('Hello Express!');
 });
